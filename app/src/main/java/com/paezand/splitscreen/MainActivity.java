7868188ade0908f39655f893175f35e8fb311b50
Paez, Andrew <andrewozumba@gmail.com>
2017-08-30 19:43:18 -0400

    Start Project

---
+++ app/src/main/java/com/paezand/splitscreen/MainActivity.java
@@ -0,0 +1,29 @@
+package com.paezand.splitscreen;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isInMultiWindowMode()) {
+
+        }
+
+        if (isInPictureInPictureMode()) {
+
+        }
+    }
+
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode, Configuration newConfig) {
+        super.onMultiWindowModeChanged(isInMultiWindowMode, newConfig);
+
+
+    }
+}
